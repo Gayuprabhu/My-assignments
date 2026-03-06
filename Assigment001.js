83de0f4955900b7c1d9594455c4f3a3df71e6fc3
Gayuprabhu <147901535+Gayuprabhu@users.noreply.github.com>
2026-03-06 12:13:12 +0530

    Add files via upload

---
+++ Assigment001.js
@@ -0,0 +1,19 @@
+
+
+function checkNum (){
+let number1=5
+let number2=-1
+let number3=0
+if(number1>=0){
+console.log ("Number is Positive" +number1);
+}
+if(number2<0){
+console.log("number is negitive" +number2);
+}
+if(number3==0){
+console.log("number is zero" +number3)
+}
+}
+
+checkNum();
+
